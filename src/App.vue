958cac111618e91d4f5bc43f9e12f4eef9e396d3
hamza99113 <alihamza99113@gmail.com>
2023-10-05 12:16:28 +0500

    Commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <base-container title="Vuex">
+    <TheCounter/>
+    <FavouriteValue/>
+    <button @click="addOne">Add 10</button>
+    <ChangeCounter/>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import FavouriteValue from './components/FavouriteValue.vue';
+ 
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCounter,
+    FavouriteValue
+  },
+  
+  methods: {
+    addOne(){
+      // this.$store.commit('increase', {value: 10});
+      this.$store.commit({
+        type: 'increase',
+        value: 10
+      });
+    }
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/FavouriteValue.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <h3>{{ counter }}</h3>
+    <p>We do more...</p>
+</div>
+</template>
+
+<script>
+export default {
+  computed: {
+    counter() {
+    //   return this.$store.state.counter;
+    //   return this.$store.getters.finalCounter;
+       return this.$store.getters.normalizedCounter;
+    },
+  },
+};
+</script>
